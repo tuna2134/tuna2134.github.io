096e3a4608412d500feede385e404bb285dc4237
tuna2134 <me@tuna2134.dev>
2023-06-06 13:13:52 +0000

    ok

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script>
+import "../app.css"
+
+let navItems = [
+    {
+        name: "Main",
+        uri: "https://tuna2134.jp"
+    },
+    {
+        name: "GitHub",
+        uri: "https://github.com/tuna2134"
+    }
+]
+</script>
+<header class="flex border-b">
+    <div class="px-2 w-full max-w-5xl mx-auto items-center flex h-16">
+        <h1 class="flex text-3xl font-bold">
+            <a href="/">tuna2134.dev</a>
+        </h1>
+        <nav class="flex ml-auto space-x-6">
+            {#each navItems as navItem}
+            <a href={navItem.uri}>{navItem.name}</a>
+            {/each}
+        </nav>
+    </div>
+</header>
+<slot />
+<style lang="postcss"></style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    let libItems = [
+        {
+            name: "voicevox-client",
+            uri: "https://github.com/voicevox-client"
+        },
+        {
+            name: "sanic-discord",
+            uri: "https://github.com/tuna2134/sanic-discord"
+        }
+    ]
+</script>
+
+<main class="px-2 w-full max-w-7xl mx-auto mt-4">
+    <div class="py-4">
+        <h2 class="text-2xl font-semibold">Library</h2>
+        <ul>
+            {#each libItems as libItem}
+            <li class="list-inside list-disc">
+                <a href={libItem.uri}>{libItem.name}</a>
+            </li>
+            {/each}
+        </ul>
+    </div>
+</main>
